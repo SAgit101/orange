a02f1dc063c9ad7e1406ae33205ea09f916abcc2
SAgit101 <Saadproject49@gmail.com>
2025-12-26 00:51:51 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "2ndstorageacct"
     container_name       = "tfstate"
-    key                  = "kaptaan/terraform.tfstate"
+    key                  = "kaptaan12/terraform.tfstate"
 
     resource_group_name  = "1resourcegroup"
     use_azuread_auth     = true
