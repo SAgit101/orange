efeabc9051ef24ee0d6ea641d169bf3cd0066dd9
SAgit101 <Saadproject49@gmail.com>
2025-12-25 19:08:02 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,19 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
     azuread = {
       source  = "hashicorp/azuread"
       version = "~> 2.0"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
   }
 }
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+provider "azuread" {}
