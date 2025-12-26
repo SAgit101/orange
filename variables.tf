8444adbbdf1cd3e657328f9710eb35db366c7d1b
SAgit101 <Saadproject49@gmail.com>
2025-12-25 19:10:43 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "sp_name" {
   description = "Service Principal name"
   type        = string
 }
+
+variable "state_key" {
+  description = "Terraform state file key"
+  type        = string
+}
